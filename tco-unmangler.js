c90921543991a1fbade50e583541eed1ec1dde0a
Na <git@amagineer.com>
2017-02-13 15:24:13 -0500

    Check if `matches` method exists before calling it
    The mutationObserver hands the callback new *text* nodes as well as DOM
    nodes, so check to make sure that the `matches` method is there before
    trying to call it.

--- tco-unmangler.js
+++ tco-unmangler.js
@@ -83,7 +83,7 @@ var desktopObserver = new MutationObserver(function(mutations){
   mutations.forEach(function(mutation) {
     mutation.addedNodes.forEach(function(node){
 
-      if (node.matches('li, div.permalink-container')){
+      if (typeof node.matches !== 'undefined' && node.matches('li, div.permalink-container')){
         // The mutations we care about are the ones that
         // insert <li>s into the DOM. Those are new tweets.
         unmangleTcoLinks(node)
